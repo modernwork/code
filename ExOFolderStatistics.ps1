fb67a681e9b7d95d0288b551a4053d45f6b2416c
modernwork <130680652+modernwork@users.noreply.github.com>
2023-04-13 10:42:27 -0600

    Add files via upload

---
+++ ExOFolderStatistics.ps1
@@ -0,0 +1,3 @@
+﻿Get-MailboxFolderStatistics -Id "x@xyz.com" | select name, FolderPath,FolderSize,ItemsInFolder | Sort-Object ItemsInFolder | export-csv C:\theFolderSize.csv
+
+#get-moverequest x@xyz.com | Get-MoveRequestStatistics -IncludeReport | fl
