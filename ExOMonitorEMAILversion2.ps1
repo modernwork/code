11a68881dff06e46f0d23337afaf5caaf695c2e7
modernwork <130680652+modernwork@users.noreply.github.com>
2023-04-13 10:44:55 -0600

    Add files via upload

---
+++ ExOMonitorEMAILversion2.ps1
@@ -0,0 +1,11 @@
+﻿$qqq = Read-Host "enter the email address to be migrated"
+
+do{
+$stats = Get-MoverequestStatistics $qqq
+
+send-mailmessage -From x@xyz.com -To x@xyz.com -Subject "$($user) $($stats.PercentComplete) % ExO move status" -SmtpServer 'smtp.xyz.com'
+
+  start-sleep -Seconds 200
+
+}until ($($stats.PercentComplete) -eq 100)
+
